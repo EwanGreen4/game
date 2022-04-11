2a79beaa28a7e549b49e795f68b7330e30ff29e1
Ewan Green <ewangreen95@gmail.com>
2022-04-11 12:03:59 -0600

    rect

--- main.js
+++ main.js
@@ -1,3 +1,35 @@
+class Point {
+	constructor(x,y) {
+		this.x = x;
+		this.y = y;
+	}
+	static x;
+	static y;
+}
+
+class Rect { //globally mapped rect
+	static left = 0;
+	static right = 0;
+	static top = 0;
+	static bottom = 0;
+	
+	constructor(x, y, w, h) {
+		this.top = y;
+		this.left = x;
+		this.bottom = y + h;
+		this.right = x + w;
+	}
+	center() { return new Point(right - (left / 2), top - (bottom / 2)); }
+	topLeft() { return new Point(left, top); }
+	topRight() { return new Point(right, top); }
+	bottomLeft() { return new Point(left, bottom); }
+	bottomRight() { return new Point(right, bottom); }
+}
+class Player {
+	static x;
+	static y;
+}
+
 document.addEventListener('keydown', function(event) {
 	switch(event.keyCode) {
 		case 37:
@@ -5,6 +37,7 @@ document.addEventListener('keydown', function(event) {
 			break;
 		case 39:
 			alert('Right was pressed');
+			
 			break;
 	}
 });
@@ -18,7 +51,11 @@ var gameArea = {
 		document.body.insertBefore(this.canvas, document.body.childNodes[0]);  
 	}  
 }  
+var img = document.createElement("img");
 
 function main() {
-	gameArea.start()
+	gameArea.start();
+
+	img.src = "resource/placeholder.png";
+	gameArea.context.drawImage(img, gameArea.width / 2, gameArea.height / 2, 16, 16);
 }
