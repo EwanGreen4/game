1f87ee1219e4730609da9424ceafb1292f7e5757
Ewan Green <ewangreen95@gmail.com>
2022-04-10 22:29:29 -0600

    yea

--- main.js
+++ main.js
@@ -8,3 +8,13 @@ document.addEventListener('keydown', function(event) {
 			break;
 	}
 });
+
+var  myGameArea = {  
+    canvas : document.createElement("canvas"),  
+    start : function() {  
+        this.canvas.width = 480;  
+        this.canvas.height = 270;  
+        thisthis.context = this.canvas.getContext("2d");  
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);  
+    }  
+}  
