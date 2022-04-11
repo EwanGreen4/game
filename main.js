dc78af62d0b9f999365fe13cac33ed684e7d9e6d
Ewan Green <ewangreen95@gmail.com>
2022-04-10 22:45:35 -0600

    sim changes

--- main.js
+++ main.js
@@ -9,12 +9,16 @@ document.addEventListener('keydown', function(event) {
 	}
 });
 
-var  myGameArea = {  
+var gameArea = {  
 	canvas : document.createElement("canvas"),  
 	start : function() {  
 		this.canvas.width = 480;  
 		this.canvas.height = 270;  
-		thisthis.context = this.canvas.getContext("2d");  
+		this.context = this.canvas.getContext("2d");  
 		document.body.insertBefore(this.canvas, document.body.childNodes[0]);  
 	}  
 }  
+
+function main() {
+	gameArea.start()
+}
