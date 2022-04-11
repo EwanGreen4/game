575985f2ee8703e36eb42fdae79245957b3ba8bb
Ewan Green <ewangreen95@gmail.com>
2022-04-10 22:38:30 -0600

    fixi

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
+
 cd ~/game
 git pull
-pkill `basename "$0"`
+
+declare -i count=0
+while [[ $(pgrep -f ${0##*/}|wc -l) -gt 1 ]]; do
+   ((++count))
+   if [[ $count -gt 10 ]]; then
+      echo "ERROR: can't kill pids" >&2
+      exit 1
+   fi
+done
+
+local mypid=$$    # capture this run's pid
+
+declare pids=($(pgrep -f ${0##*/}   # get all the pids running this script
+
+for pid in ${pids[@]/$mypid/}; do   # cycle through all pids except this one
+	kill $pid                        # kill the other pids
+	sleep 1                          # give time to complete
+done
+
 nohup npx serve -p 6621 &
 
