f4ca1a8c0728ed666ddf30a6b36467a8b43d42a8
Ewan Green <ewangreen95@gmail.com>
2022-04-11 21:56:10 -0600

    super simple main menu

--- main.js
+++ main.js
@@ -1,34 +1,12 @@
-class Point {
-	constructor(x,y) {
-		this.x = x;
-		this.y = y;
+function include() {
+	for (var i = 0; i < arguments.length; i++) {
+		var script  = document.createElement('script');
+		script.src = arguments[i];
+		document.getElementsByTagName('head').item(0).appendChild(script);
 	}
-	static x;
-	static y;
 }
 
-class Rect { //globally mapped rect
-	static left = 0;
-	static right = 0;
-	static top = 0;
-	static bottom = 0;
-	
-	constructor(x, y, w, h) {
-		this.top = y;
-		this.left = x;
-		this.bottom = y + h;
-		this.right = x + w;
-	}
-	center() { return new Point(right - (left / 2), top - (bottom / 2)); }
-	topLeft() { return new Point(left, top); }
-	topRight() { return new Point(right, top); }
-	bottomLeft() { return new Point(left, bottom); }
-	bottomRight() { return new Point(right, bottom); }
-}
-class Player {
-	static x;
-	static y;
-}
+include('types.js', 'menu.js');
 
 document.addEventListener('keydown', function(event) {
 	switch(event.keyCode) {
@@ -43,15 +21,12 @@ document.addEventListener('keydown', function(event) {
 });
 
 function main() {
-    var canvas = document.getElementById('canvas');
-    canvas.width = 500;
-    canvas.height = 500;
-    var ctx = canvas.getContext('2d');
 
-    var img = new Image(16, 16);
-    img.src = 'resource/placeholder.png';
-    img.onload = function() {
-        ctx.drawImage(img, canvas.width/2, canvas.height/2);
-    }
+	var canvas = document.getElementById('canvas');
+	canvas.width = 640;
+	canvas.height = 440;
+	var ctx = canvas.getContext('2d');
 	//gameArea.context.drawImage(img, gameArea.width / 2, gameArea.height / 2);
+
+	loadMainMenu();
 }
---
+++ menu.js
@@ -0,0 +1,40 @@
+function loadMainMenu() {
+	var canvas = document.getElementById('canvas');
+    var ctx = canvas.getContext('2d');
+
+	ctx.fillStyle = '#ddffdd';
+	ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+	ctx.fillStyle = '#000000';
+	ctx.font = "bold 48px serif";
+	var text = "This is the game";
+	var textMetrics = ctx.measureText(text);
+	ctx.fillText(text, canvas.width / 2 - textMetrics.width / 2, canvas.height / 3);
+	
+	var height = canvas.height / 3 * 2;
+	var imgNew = new Image;
+	imgNew.src = 'resource/menuNew.png';
+	imgNew.onload = function() {
+		ctx.drawImage(this, canvas.width / 3 - this.width / 2, height - this.height / 2);
+	}
+	var imgLoad = new Image;
+	imgLoad.src = 'resource/menuLoad.png';
+	imgLoad.onload = function() {
+		ctx.drawImage(this, (canvas.width / 3 * 2)- this.width / 2, height - this.height / 2);
+	}
+	var imgSettings = new Image;
+	imgSettings.src = 'resource/menuSettings.png';
+	imgSettings.onload = function() {
+		ctx.drawImage(this, canvas.width / 2 - this.width / 2, height - this.height / 2);
+	}
+// 	imgSettings.onclick = loadSettingsMenu()
+	
+}
+
+function loadSettingsMenu() {
+	alert("Settings menu")
+}
+
+function loadInventoryMenu() {
+	
+}
