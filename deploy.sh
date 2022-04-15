d2b51071a980d94e8404ebd3768447e7545851c1
Ewan Green <ewangreen95@gmail.com>
2022-04-14 23:27:37 -0600

    add minification

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,9 @@
 
 cd ~/game
 git pull
+for i in $(find . -type f -name "*.js" ! -name "*.min.*")
+do
+	npx minify "$i" > "${i%.*}.min.js"
+done
 printf "Running server."
 npx serve -p 6621 > /dev/null
