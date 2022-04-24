7880f5bf29d4ce5da24b0e8d7cb302dbd58ca637
Ewan Green <ewangreen95@gmail.com>
2022-04-24 16:34:20 -0600

    cleane

--- deploy/format.sh
+++ deploy/format.sh
@@ -8,8 +8,6 @@ source "$(dirname $(realpath $0))/common.sh"
 
 for i in $(find "$baseDirectory" -maxdepth 1 -type f -name "*.js" -o -name "*.html" ! -name "*.min.*") # Potentially CSS in the future; not using any right now
 do
-	basename="$(basename $i)"
-	filename="$instanceFolder/$basename"
-	printf "Formatting file \'$(fileNameColorWrap $basename)\'.\n"
+	printf "Formatting file \'$(fileNameColorWrap $(basename $i))\'.\n"
 	npx prettier --config "$deployFolder/.prettier.json" --write "$i" > /dev/null
 done
