4e2c65e3f38f366452a0ac9270a9f5dc406ebb18
Ewan Green <ewangreen95@gmail.com>
2022-04-24 14:54:23 -0600

    small cleanup

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 
 # Script for deploying the game as a static webpage. Makes no alterations to
 # the existing code (except for formatting it). It can be tested locally 
-# (within browser with non-minified versions) as well as deployed.
+# (within browser with non-minified, original versions) as well as "deployed".
 
 # This script will do the following:
 #		- Format source files
@@ -15,37 +15,38 @@
 cd ~/game
 git pull
 
-deployFolder="instance"
+deployFolderName="instance"
+deployFolder="$(pwd)/$deployFolderName"
 
-if [ -d "./$deployFolder" ]
+if [ -d "$deployFolder" ]
 then
-	rm -rdf ./$deployFolder
+	rm -rdf "$deployFolder"
 fi
-mkdir ./$deployFolder
+mkdir "$deployFolder"
 
-for i in $(find . -maxdepth 1 -type d ! -name ".*" ! -name "$deployFolder" ! -name "deploy")
+for i in $(find . -maxdepth 1 -type d ! -name ".*" ! -name "$deployFolderName" ! -name "deploy")
 do
 	name=$(basename $i)
 	printf "Creating symlink for directory \'\e[34m$(basename $i)\e[0m\' (\'\e[34m$deployFolder/$(basename $i)\e[0m\').\n"
-	ln -s "$(pwd)/$name" "./$deployFolder/$name"
+	ln -s "$(pwd)/$name" "$deployFolder/$name"
 done
 
 for i in $(find . -maxdepth 1 -type f -name "*.js" -o -name "*.html" ! -name "*.min.*")
 do
 	printf "Processing file \'\e[34m$(basename $i)\e[0m\'.\n"
 	npx prettier --write "$i" > /dev/null
-	python3 "./deploy/min-names.py" "$i" > "./$deployFolder/$i"
+	python3 "./deploy/min-names.py" "$i" > "$deployFolder/$i"
 	if [[ $i == *html ]] # .min.html does not work
 	then
-		tmp=$(npx minify "./$deployFolder/$i")
-		echo $tmp > "./$deployFolder/$i"
+		tmp=$(npx minify "$deployFolder/$i")
+		echo $tmp > "$deployFolder/$i"
 	else {
-		npx minify "./$deployFolder/$i" > "./$deployFolder/${i%.*}.min.js"
-		rm "./$deployFolder/$i"
+		npx minify "$deployFolder/$i" > "$deployFolder/${i%.*}.min.js"
+		rm "$deployFolder/$i"
 	}
 	fi
 done
 
 printf "Running server.\n"
-cd ./$deployFolder
+cd $deployFolder
 npx serve -p 6621 > /dev/null
