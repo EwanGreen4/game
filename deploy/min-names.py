dd57d6471b62f508408a936a5c1ec881f5ffa949
Ewan Green <ewangreen95@gmail.com>
2022-04-24 16:52:35 -0600

    clean more

--- deploy/min-names.py
+++ deploy/min-names.py
@@ -10,23 +10,18 @@
 import re
 import sys
 
-delim = "ඞඞඞ" 
-
-with open(sys.argv[1], "r") as f:
-    data = f.read().replace('\n', delim)
+if len(sys.argv) > 1:
+    delim = "ඞඞඞ"  # eh
+    with open(sys.argv[1], "r") as f:
+        data = f.read().replace('\n', delim)
+        
+    x = re.findall("MIN-START(.*?)MIN-END", data) # Must use non-greedy matching or it will match all between first MIN-START and last MIN-END (bad)
+    if x != None:
+        for i in x:
+            postStr = re.sub(r'\w*(?<!\.min\.)js', 'min.js', i).replace(delim, '\n')
+            data = data.replace(i, postStr).replace(delim, '\n')
     
-x = re.findall("MIN-START(.*?)MIN-END", data) # Must use non-greedy matching or it will match all between first MIN-START and last MIN-END (bad)
-
-if x is not None:
+    print(data)
     
-    for i in x:
-        preStr = i
-
-        postStr = re.sub(r'\w*(?<!\.min\.)js', 'min.js', preStr)
-        postStr = postStr.replace(delim, '\n')
-
-        data = data.replace(preStr, postStr)
-
-data = data.replace(delim, '\n')
-
-print(data)
+else:
+    sys.exit('You must provide a file.')
