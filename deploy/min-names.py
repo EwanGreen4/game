9b1ffdb82638437c626d3c55a3d78256115e67ab
Ewan Green <ewangreen95@gmail.com>
2022-04-24 18:30:16 -0600

    eehaheva

--- deploy/min-names.py
+++ deploy/min-names.py
@@ -18,10 +18,8 @@ if len(sys.argv) > 1:
     x = re.findall("MIN-START(.*?)MIN-END", data) # Must use non-greedy matching or it will match all between first MIN-START and last MIN-END (bad)
     if x != None:
         for i in x:
-            postStr = re.sub(r'\w*(?<!\.min\.)js', 'min.js', i)
-            postStr = postStr.replace(delim, '\n')
-            data = data.replace(i, postStr)
-            data = data.replace(delim, '\n')
+            postStr = re.sub(r'\w*(?<!\.min\.)js', 'min.js', i).replace(delim, '\n')
+            data = data.replace(i, postStr).replace(delim, '\n')
     
     print(data)
     
